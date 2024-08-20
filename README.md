1fb5a9ad08bbf970d0082ff73563ffe4216f9947
CHIRANJEET DWIVEDY <154692196+CHIRANJEET12@users.noreply.github.com>
2024-08-20 21:45:48 +0530

    commit
    initial

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # SMART_HOSTLE_SYSTEM
+Setting up the client as of now --
+command line- npm run dev to simple run the frontend part as of now
---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState,React } from 'react'
+import Navbar from './components/Navbar'
+function App() {
+
+  return (
+    <>
+      <Navbar></Navbar>
+    </>
+  )
+}
+
+export default App
